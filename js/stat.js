90dc06acf7ef92ed6720da42e1a3585a1b3087c6
Anna-Anastasiya Raulets <raulets2@gmail.com>
2019-09-23 22:54:12 +0100

    Задание 2. Переделаны переменные в формулы

--- js/stat.js
+++ js/stat.js
@@ -2,18 +2,30 @@
 
 var CLOUD_WIDTH = 420; // ширина облака
 var CLOUD_HEIGHT = 270; // высота облака
-var CLOUD_GAP_TOP = 10; // отступ в облаке
-var SHADOW_GAP_LEFT = 110; // отступ слева для тени
-var SHADOW_GAP_TOP = 20; // отступ сверху для тени
-var TEXT_GAP_LEFT = 120; // отступ слева для текста
-var TEXT_LINE1 = 40; // отступ в тексте 1 строка
-var TEXT_LINE2 = 60; // отступ в тексте 2 строка
-var TEXT_GAP = 100; // отступ для текста слева
-var HIST_GAP = 150; // отступ для имен слева
-var HIST_OFFSET = 90; // отступ между колонок для имен
-var TOP_GAP_NAME = 260; // отступ сверху для имени
-var TOP_GAP_SEC = 90; // отступ сверху для милисекунд
-var HIST_MAX_HEIGHT = 130;
+
+var CLOUD_X = 100; // координата -----горизонталь-----
+var CLOUD_Y = 10; // координата -------вертикаль----------
+var SHADOW_X = CLOUD_X + 10; // отступ для тени X
+var SHADOW_Y = CLOUD_Y + 10; // отступ для тени Y
+
+var MAIN_TEXT_X = CLOUD_X + 20; // отступ слева для текста
+var MAIN_TEXT_LINE1_Y = CLOUD_Y + 30; // отступ в тексте 1 строка
+var MAIN_TEXT_LINE2_Y = MAIN_TEXT_LINE1_Y + 20; // отступ в тексте 2 строка
+
+// Гистограмма X
+var HIST_X = CLOUD_X + 50;
+var BAR_WIDTH = 40;
+var BAR_GAP = 50;
+var HIST_OFFSET = BAR_WIDTH + BAR_GAP;
+
+// Гистограмма Y
+var HIST_Y = CLOUD_Y + 80;
+var BAR_MAX_HEIGHT = 130;
+var HIST_SCORES_GAP = 10;
+
+var BAR_START_Y = HIST_Y + HIST_SCORES_GAP; // Начало столбика сверху (10 отступ на очки)
+var HIST_NAMES_Y = HIST_Y + BAR_MAX_HEIGHT + HIST_SCORES_GAP + 20; // отступ сверху для имени
+
 
 var arrayMax = function (a) {
   var result = -1;
@@ -31,34 +43,34 @@ window.renderStatistics = function (ctx, names, times) {
 
   // тень
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(SHADOW_GAP_LEFT, SHADOW_GAP_TOP, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillRect(SHADOW_X, SHADOW_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
 
   // облако
   ctx.fillStyle = '#fff';
-  ctx.fillRect(TEXT_GAP, CLOUD_GAP_TOP, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillRect(CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
 
   // Обводка
   ctx.strokeStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.strokeRect(TEXT_GAP, CLOUD_GAP_TOP, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.strokeRect(CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
 
   // Шрифт
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', TEXT_GAP_LEFT, TEXT_LINE1);
-  ctx.fillText('Список результатов:', TEXT_GAP_LEFT, TEXT_LINE2);
+  ctx.fillText('Ура вы победили!', MAIN_TEXT_X, MAIN_TEXT_LINE1_Y);
+  ctx.fillText('Список результатов:', MAIN_TEXT_X, MAIN_TEXT_LINE2_Y);
 
   // вычисление миллисекунд
   var maxTime = arrayMax(times);
-  var ms2px = HIST_MAX_HEIGHT / maxTime;
+  var ms2px = BAR_MAX_HEIGHT / maxTime;
 
   // вычисление имен
   for (i = 0; i < names.length; i++) {
     var barHeight = times[i] * ms2px;
-    var extraVerticalGap = HIST_MAX_HEIGHT - barHeight;
+    var extraVerticalGap = BAR_MAX_HEIGHT - barHeight; // магический отступ что бы столюики сместились вниз
 
     // пишем миллисекунды
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), HIST_GAP + i * HIST_OFFSET, TOP_GAP_SEC + extraVerticalGap);
+    ctx.fillText(Math.round(times[i]), HIST_X + i * HIST_OFFSET, HIST_Y + extraVerticalGap);
 
     // Рандом столбиков
     if (names[i] === 'Вы') {
@@ -68,10 +80,10 @@ window.renderStatistics = function (ctx, names, times) {
     }
 
     // столбики
-    ctx.fillRect(HIST_GAP + i * HIST_OFFSET, 100 + extraVerticalGap, 40, barHeight);
+    ctx.fillRect(HIST_X + i * HIST_OFFSET, BAR_START_Y + extraVerticalGap, BAR_WIDTH, barHeight);
 
     // пишем имена
     ctx.fillStyle = '#000';
-    ctx.fillText(names[i], HIST_GAP + i * HIST_OFFSET, TOP_GAP_NAME);
+    ctx.fillText(names[i], HIST_X + i * HIST_OFFSET, HIST_NAMES_Y);
   }
 };
